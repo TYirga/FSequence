bc8d44dee0c2c57dcbe0ed0e8e7f5677d1e138e6
tizazu <esyan4>
2018-01-29 11:20:12 -0500

    Printing Correctly

--- src/main/java/tize/com/example/demo/FibonacciNumber.java
+++ src/main/java/tize/com/example/demo/FibonacciNumber.java
@@ -7,23 +7,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FibonacciNumber {
     @RequestMapping("/")
-    public String Fnumber(@RequestParam("num") int num) {
+    public String Fnumber(@RequestParam("num") int num, @RequestParam("count") int count) {
 
         String f_number = "";
-        int al = num;
-
-
-        f_number+="<br/>" +num;
-        int a2 =num+2;
+        int a1 = 0;
+        f_number+="<br/>" +a1;
+        int a2 =0;
         f_number+="<br/>" + a2;
-        int a3 = 1+a2;
+        int a3 = 1;
         f_number+="<br/>" + a3;
-        int a4=num+a2+a3;
+        int a4=num;
         f_number+="<br/>" + a4;
-        for(int i=0;i<=5;i++){
-
-        int output = num+a2+a3+a4;
-        f_number+="<br/>" + output;}
+        for(int i=0;i<count;i++){
+         int a5=a1+a2+a3+a4;
+            f_number+="<br/>" +a5;
+            a1=a2;
+            a2=a3;
+            a3=a4;
+            a4=a5;}
             return f_number;
 
 
